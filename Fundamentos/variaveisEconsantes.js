89a94367dd664bf1f0f448702903acd3c9520402
André Carvalho <andredeco2003@gmail.com>
2024-06-11 09:15:46 -0300

    Variáveis e constantes

---
+++ Fundamentos/variaveisEconsantes.js
@@ -0,0 +1,8 @@
+var a = 3;
+console.log(a);  // ultrapasado
+
+let b = 6;
+console.log(b); // moderno
+
+const c = 50; // constantes não mudam \ valores esaticos \ , em programação funcional sao amplamente usadas
+console.log(c);
