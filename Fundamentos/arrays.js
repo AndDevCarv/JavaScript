a024b0370a3f49ff2b3b79b8e122b273cc15f082
André Carvalho <andredeco2003@gmail.com>
2024-06-14 17:58:53 -0300

    Arrays
    iniciando com arrays

---
+++ Fundamentos/arrays.js
@@ -0,0 +1,13 @@
+const valores = [1,2,'a',4,'b'];
+console.log(valores[2], valores[4]);
+valores[4] = 'c';
+console.log(valores[2], valores[4])
+
+valores.push(5, false, 8, true);
+console.log(valores);
+
+console.log(valores.pop());
+console.log(valores);
+
+delete valores[0];
+console.log(valores);
