7e15f39a04fca13e995a442a6b779ad6ac790a38
André Carvalho <andredeco2003@gmail.com>
2024-07-02 22:42:32 -0300

    testes

--- Funcoes/callbacks.js
+++ Funcoes/callbacks.js
@@ -1,12 +1,19 @@
 const notas = [6.7, 5.5, 3.1, 8.9, 10, 6.3, 8.5]
 
 
-let notasBaixas = [];
+let notasBaixas1 = [];
 
 for (let i in notas) {
     if (notas[i] < 7){
-        notasBaixas.push(notas[i]);
+        notasBaixas1.push(notas[i]);
     }   
 }
 
-console.log(notasBaixas);
+console.log(notasBaixas1);
+
+
+let notasBaixas2 = notas.filter(function (nota) {
+    return nota < 7;
+})
+
+console.log(notasBaixas2);
