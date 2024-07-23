6ad49ba2ae30a51ca7290f492c56561fa2c5df3e
André Carvalho <andredeco2003@gmail.com>
2024-07-23 16:19:39 -0300

    exercicios

--- Exercícios/ex27.js
+++ Exercícios/ex27.js
@@ -7,22 +7,29 @@ let criancas =
 
 function calculaAltura(criancas, anos) {
 
-    for (i = 0; i < criancas.length; i++) {
+    for (i = 0; i < criancas.length - 1; i++) {
         let crianca1 = criancas[i]
-        let crianca2 = criancas[i+1]
+        let crianca2 = criancas[i + 1]
 
         if (crianca1.altura > crianca2.altura) {
-            console.log(`A crianca1 é maior que a 2`);
+            console.log(`A crianca ${crianca1.nome} é maior que a crianca ${crianca2.nome}`);
+            if (crianca2.crescimento > crianca1.crescimento) {
+                while (crianca1.altura > crianca2.altura) {
+                    let crianca2 = criancas[i]
+                    crianca2.altura += crianca.crescimento * anos
+                }
+            }
+        } else if (crianca1.altura < crianca2.altura) {
+            console.log(`A crianca ${crianca2.nome} é maior que a crianca ${crianca1.nome}`);
+        } else {
+            console.log('As duas criancas tem a mesma altura');
         }
-    }
 
-    for(i = 0; i < criancas.length; i++) {
         let crianca = criancas[i]
         crianca.altura += crianca.crescimento * anos
-       
+
+        return console.log(`Em ${anos} anos as criancas vão ter: ${crianca1.nome} ${crianca1.altura} e ${crianca2.nome} ${crianca2.altura}`);
     }
-    return criancas
-    
 
 }
 
