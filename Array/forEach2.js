7508d7659fc0f18d8ee1d7607674fcf313260239
André Carvalho <andredeco2003@gmail.com>
2024-08-22 22:40:58 -0300

    foreach manual

---
+++ Array/forEach2.js
@@ -0,0 +1,11 @@
+function forEach2(lista, callback) {
+    for (let i = 0; i < lista.length; i++){
+        
+    }
+}
+
+const aprovados = ['andre', 'bruno', 'luana']
+
+aprovados.forEach2(function(nome, indice){ 
+    console.log(`${nome}, ${indice}`);
+})
