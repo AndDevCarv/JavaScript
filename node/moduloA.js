3bfe9a3bfbc462593a2f2f0aec65e74b645664f3
André Carvalho <andredeco2003@gmail.com>
2024-09-23 21:42:12 -0300

    iniciando o node

---
+++ node/moduloA.js
@@ -0,0 +1,8 @@
+this.ola = 'fala ai';
+exports.bemVindo = 'Bem vindo ao node!';
+exports.ateLogo = 'Até o próximo exemplo';
+module.exports.ateLogoo = 'Até o próximo exemplo';
+
+let tchau = 'tchau'
+
+module.exports.tchau
