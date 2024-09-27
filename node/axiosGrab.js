1624284a7e81a8d179e6f443aa6960c3b7ebbf80
André Carvalho <andredeco2003@gmail.com>
2024-09-26 21:44:33 -0300

    axio up

--- node/axiosGrab.js
+++ node/axiosGrab.js
@@ -1,11 +1,18 @@
 const url = "http://files.cod3r.com.br/curso-js/funcionarios.json"
 
 const axios = require('axios')
+const { filter } = require('lodash')
 
 axios.get(url).then(resposta => {
     const funcionarios = resposta.data
+    const funcionariosChing = funcionarios.filter(pessoa => pessoa.pais === 'China')
+    const molieres = funcionariosChing.filter(pessoa => pessoa.genero === 'F')
+
+    const maiorSalario = molieres.reduce((acumulador, pessoaAtual)=> {
+        return acumulador.salario < pessoaAtual.salario ? acumulador : pessoaAtual
+    })
+
+    console.log(maiorSalario);
     
-    const nomes = 
-    funcionarios.filter (pessoa => pessoa.genero === 'F').map(pessoa => ({...pessoa}))
     
 })
