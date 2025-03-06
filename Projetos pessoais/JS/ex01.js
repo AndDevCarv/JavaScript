88334f850b4b9445319ae796f11982ef8682e9ae
Andre <andredeco2003@gmail.com>
2025-03-05 21:51:17 -0300

    inicio dos exercicios

---
+++ Projetos pessoais/JS/ex01.js
@@ -0,0 +1,7 @@
+function operacao (valor1, valor2) {
+    console.log(valor1 + valor2);
+    console.log(valor1 - valor2);
+    console.log(valor1 * valor2);
+    console.log(valor1 / valor2);
+}
+operacao(10, 5)
