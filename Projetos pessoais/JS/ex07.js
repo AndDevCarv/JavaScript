ab6554925ce06548b9774c05f7f3dca074b72068
Andre <andredeco2003@gmail.com>
2025-03-11 21:51:27 -0300

    bhaskara

--- Projetos pessoais/JS/ex07.js
+++ Projetos pessoais/JS/ex07.js
@@ -1,7 +1,20 @@
 function bhaskara (a,b,c) {
-    let delta = (b ** 2) - 4 * a * c;
+    let delta = (b ** 2) - (4 * a * c);
 
-    return delta
+    if (delta < 0) {
+        return console.log("Delta é negativo");
+    }
+
+    let x2 = ((-b - Math.sqrt(delta)) / (2 * a))
+    let x1 = ((-b + Math.sqrt(delta)) / (2 * a));
+    
+    let resultado = [];
+    resultado.push(x1);
+    resultado.push(x2)
+
+    return resultado
 }
 
-console.log(bhaskara(2, -3, -2));
+console.log(bhaskara(2,-3,-2));
+
+
