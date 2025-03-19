570d5d6445ced549ef8807628a5c084de37ec9b2
Andre <andredeco2003@gmail.com>
2025-03-18 21:53:28 -0300

    operações switch

---
+++ Projetos pessoais/JS/20.js
@@ -0,0 +1,9 @@
+function notas (n) {
+    let nota100 = 0
+    let nota50 = 0
+    let nota10 = 0
+    let nota5 = 0
+    let nota1 = 0
+
+        
+}
---
+++ Projetos pessoais/JS/ex18.js
@@ -0,0 +1,38 @@
+function extenso (n) {
+    switch (n) {
+        case 1:
+            console.log('Um');
+            break
+        case 2:
+            console.log('dois');
+            break
+        case 3:
+            console.log('tres');
+            break
+        case 4:
+            console.log('quatro');
+            break
+        case 5:
+            console.log('cinco');
+            break
+        case 6:
+            console.log('seis');
+            break
+        case 7:
+            console.log('sete');
+            break
+        case 8:
+            console.log('oito');
+            break
+        case 9:
+            console.log('nova');
+            break
+        case 10:
+            console.log('dez');  
+            break
+        default:
+            console.log("Inválido");
+    }
+}
+
+extenso(8)
