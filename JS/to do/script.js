d0c05be4e751249f1a42eed4021ae6e43092595b
Andre <andredeco2003@gmail.com>
2025-04-09 22:29:22 -0300

    js pronto

--- JS/to do/script.js
+++ JS/to do/script.js
@@ -1,11 +1,10 @@
 function addTask () {
     const inputTask = document.querySelector("#inputTask");
-    const task = inputTask.ariaValueMax.trim();
+    const task = inputTask.value.trim();
 
     if (task == "") return;
 
-    const li = document.createElement("li");
-    li.textContent = task;
+    const li = createElementTask(task);
 
     const taskList = document.querySelector("#taskList");
     taskList.appendChild(li)
@@ -15,6 +14,45 @@ function addTask () {
     inputTask.value = "";
 }
 
-function localStorageSave () {
-    
+function createElementTask(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    const button = document.createElement("button");
+    button.textContent = "Excluir";
+    button.className = "deleteBtn";
+
+    button.addEventListener("click", function() {
+        li.remove();
+        localStorageRemove(task);   
+    });
+
+    li.appendChild(button);
+    return li;
 }
+
+function localStorageSave (task) {
+    let savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    savedTasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+}
+
+function loadSavedTasks () {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tasks.forEach(task => {
+        const li = createElementTask(task);
+        document.getElementById("taskList").appendChild(li);
+    });
+}
+
+function localStorageRemove(taskToRemove) {
+    let savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    savedTasks = savedTasks.filter(task => task !== taskToRemove);
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+}
+
+document.querySelector("#addTask").addEventListener("click", addTask);
+
+
+loadSavedTasks();
