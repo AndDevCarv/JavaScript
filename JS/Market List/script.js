692853f56c01866f92317741ac4b565d187b52cd
Andre <andredeco2003@gmail.com>
2025-04-21 14:35:30 -0300

    market

--- JS/Market List/script.js
+++ JS/Market List/script.js
@@ -1,5 +1,52 @@
 function addItem () {
-    const inputItem = document.querySelector 
+    const inputItem = document.querySelector("#inputItem").value.trim();
+    const inputNum = document.querySelector("#inputNum").value.trim();
+    
 
+    if (inputItem == "" || inputNum == "" || isNaN(inputNum)) return;
+    
+    const item = {
+        nome: inputItem,
+        quantidade: inputNum,
+        comprado: false
+    }
+    const created = createItem(item);
 
+    const needList = document.querySelector("#needToBuy");
+    needList.appendChild(created);
+
+    localStorageSave(created)
+}
+
+function createItem(item) {
+    const li = document.createElement("li");
+    li.textContent = `${item.nome} - ${item.quantidade}`;
+
+    const button = document.createElement("button");
+    button.textContent = "Comprado";
+    button.className = "buyButton";
+
+    button.addEventListener(cancelIdleCallback, function () {
+        li.purchased()
+        const purchasedList = document.querySelector("#purchased");
+        purchasedList.appendChild(li);
+
+    })
+
+    li.appendChild(button);
+}
+
+function localStorageSave(item) {
+    let savedItem = JSON.parse(localStorage.getItem("item")) || [];
+    savedItem.push(item);
+    localStorage.setItem("item", JSON.stringify(savedItem));
+}
+
+function loadSavedItem() {
+    let item = JSON.parse(localStorage.getItem("item")) || [];
+
+    item.forEach(item => {
+        const li = createItem(item);
+        document.getElementById("needToBuy").appendChild(li);
+    });
 }
