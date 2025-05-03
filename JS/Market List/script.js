56c87eadfd1764593cf2affb0e7039bd10083342
AndDevCarv <andredeco2003@gmail.com>
2025-05-02 22:55:21 -0300

    sextou

--- JS/Market List/script.js
+++ JS/Market List/script.js
@@ -1,30 +1,30 @@
 function addItem () {
-    const inputItem = document.querySelector("#inputItem").value.trim();
-    const inputNum = document.querySelector("#inputNum").value.trim();
+    const inputItem = document.querySelector("#inputItem").value.trim(); //busca o nome do item
+    const inputNum = document.querySelector("#inputNum").value.trim(); //busca a quantidade dos itens
     
 
-    if (inputItem == "" || inputNum == "" || isNaN(inputNum)) return;
+    if (inputItem == "" || inputNum == "" || isNaN(inputNum)) return; //se o item for vazio ou negativo, retorna
     
     const item = {
         nome: inputItem,
-        quantidade: inputNum,
+        quantidade: inputNum, //constroi o objeto
         comprado: false
     }
     const created = createItem(item);
 
-    const needList = document.querySelector("#needToBuy");
-    needList.appendChild(created);
+    const needList = document.querySelector("#needToBuy"); //busca a coluna dos itens a serem comprados
+    needList.appendChild(created); //insere no html o novo item
 
-    localStorageSave(item)
-    document.querySelector("#inputItem").value = "";
+    localStorageSave(item) //salva no storage
+    document.querySelector("#inputItem").value = ""; //retorna os valores para os iniciais
     document.getElementById("inputNum").value = "";
 }
 
 function createItem(item) {
     const li = document.createElement("li");
-    li.textContent = `${item.nome} - ${item.quantidade}`;
+    li.textContent = `${item.nome} - ${item.quantidade}`; //cria o esqueleto
 
-    const button = addButtonPurchased(li);
+    const button = addButtonPurchased(li); //adicina o botao
 
     li.appendChild(button);
     
@@ -33,9 +33,9 @@ function createItem(item) {
 
 
 function localStorageSave(item) {
-    let savedItem = JSON.parse(localStorage.getItem("item")) || [];
-    savedItem.push(item);
-    localStorage.setItem("item", JSON.stringify(savedItem));
+    let savedItem = JSON.parse(localStorage.getItem("item")) || []; //busca no storage e tira do modo json "tira da gaveta"
+    savedItem.push(item); //insere o item
+    localStorage.setItem("item", JSON.stringify(savedItem)); //retorna pro storage na forma de json "coloca na gaveta"
 }
 
 function loadSavedItem() {
@@ -43,26 +43,50 @@ function loadSavedItem() {
 
     item.forEach(item => {
         const li = createItem(item);
-        document.getElementById("needToBuy").appendChild(li);
+
+        if (item.comprado) {
+            document.getElementById("purchased").appendChild(li);
+            li.removeChild(li.querySelector(".buyButton"))
+        } else {
+            document.getElementById("needToBuy").appendChild(li)
+        }
     });
 }
 
 function addButtonPurchased(li) {
     const buttonPuchased = document.createElement("button");
     buttonPuchased.textContent = "Comprado";
-    buttonPuchased.className = "buyButton";
+    buttonPuchased.className = "buyButton"; //vai criar o elemento
 
     buttonPuchased.addEventListener("click", function() {
-        li.remove();
-        li.removeChild(buttonPuchased);
-        const purchasedList = document.querySelector("#purchased");
-        purchasedList.appendChild(li);
+        li.remove(); //quando clicado vai remover o li do html
+        li.removeChild(buttonPuchased); //vai remover o comprado
+        updateLocalStorage({
+            nome: li.textContent.split("-")[0], quantidade: li.textContent.split("-")[1]
+        })
         
     })
 
     return buttonPuchased;
 }
 
+function updateLocalStorage(updatedItem) {
+    let items = JSON.parse(localStorage.getItem("item")) || []
+
+    items = items.map(item => {
+        if (
+            item.nome.trim() === updatedItem.nome.trim() &&
+            item.quantidade.trim() === updatedItem.quantidade.trim()
+        ) {
+            return {...item, comprado: true}
+        }
+
+        return item
+    });
+
+    localStorage.setItem("item", JSON.stringify(items));
+    loadSavedItem();
+}
 
 
 document.getElementById("addButton").addEventListener("click", addItem);
