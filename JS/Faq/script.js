520d85d68c58cacfba9ec56aaeb3e6de6d134d6d
AndDevCarv <andredeco2003@gmail.com>
2025-06-03 22:19:26 -0300

    retorno

--- JS/Faq/script.js
+++ JS/Faq/script.js
@@ -1,8 +1,22 @@
-const faqButtons = document.querySelectorAll(".faqBtn");
+const faqButtons = document.querySelectorAll(".faqBtn"); //pesquisa o botão, observe que, ele usa o selectorAll que retorna uma lista de todos os elementos com aquela tag
 
-faqButtons.forEach(button => {
+faqButtons.forEach(button => { //foreach que vai percorrer todos os itens dessa lista
     button.addEventListener("click", function() {
-        const postIt = button.nextElementSibling;
-        postIt.classList.toggle("show")
+        const postIt = button.nextElementSibling; //quando clicar no BOTAO, ele vai pegar o proximo elemento, nesse caso o paragrafo
+        postIt.classList.toggle("show") //após pegar o parágrafo ele aciona a classe .show, se ela ja estiver acionada ele desativa
     })
 });
+
+/*resumo que a ia mandou:
+Quando usar cada um?
+Usa querySelector quando você só precisa de um único elemento (ex: um menu, um modal, etc).
+
+Usa querySelectorAll quando você precisa aplicar alguma lógica em vários elementos, como:
+
+Vários botões de FAQ
+
+Vários campos de formulário
+
+Várias imagens, etc.
+
+ */
