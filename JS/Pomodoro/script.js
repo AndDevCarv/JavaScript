9a2c48583bdcbb51a1ac68c4fb930e8534afc057
AndDevCarv <andredeco2003@gmail.com>
2025-06-05 21:31:01 -0300

    logica cronometro pronta

--- JS/Pomodoro/script.js
+++ JS/Pomodoro/script.js
@@ -1,6 +1,8 @@
 const secondElement = document.getElementById("second");
 const minuteElement = document.getElementById("minute");
-const startButton = document.getElementById("start")
+const startButton = document.getElementById("start");
+const resetButton = document.getElementById("reset");
+const pauseButton = document.getElementById("pause");
 
 let second = 0
 let minute = 0
@@ -10,14 +12,31 @@ function timer() {
     if (second == 60) {
         second = 0
         minute++
-        minuteElement.textContent = minute;
+        minuteElement.textContent = String(minute).padStart(2, "0");
     }
-    secondElement.textContent = second;
+    secondElement.textContent = String(second).padStart(2, "0");
 }
 
 let cron;
 function start() {
+    clearInterval(cron)
     cron = setInterval(timer, 1000)
 }
 
+function reset() {
+    clearInterval(cron)
+    clearInterval(timer)
+    second = 0
+    minute = 0
+    minuteElement.textContent = "00"
+    secondElement.textContent = "00"
+}
+
+function pause() {
+    clearInterval(cron)
+    clearInterval(timer)
+}
+
 startButton.addEventListener("click", start);
+resetButton.addEventListener("click", reset);
+pauseButton.addEventListener("click", pause);
