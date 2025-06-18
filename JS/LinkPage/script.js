56ad47f89a3c330a731fea899dab1e6837a41b33
AndDevCarv <andredeco2003@gmail.com>
2025-06-18 13:08:38 -0300

    indo

--- JS/LinkPage/script.js
+++ JS/LinkPage/script.js
@@ -22,9 +22,27 @@ const links = {
 function updateLink() {
     const selectedOS = document.getElementById("select-os").value;
     const typedPrinter = document.getElementById("input-printer").value.toLowerCase().trim();
-    const linkItem = document.getElementById("link-item");
+    const allLinks = document.querySelectorAll(".link-item");
 
     if (selectedOS && typedPrinter) {
-        linkItem.href = links[selectedOS][typedPrinter] || "#";
+        allLinks.href = links[selectedOS][typedPrinter] || "#";
     }
+}
+
+function searchPrint() {
+    document.getElementById("select-print").addEventListener("input", function() {
+        const searchItem = this.value.trim().toLowerCase();
+        const printList = document.querySelectorAll("#printer-list li");
+
+        printList.forEach(item => {
+            const text = item.textContent.toLowerCase();
+
+            if (text.includes(searchItem)) {
+                item.style.display = "list-item"
+            }
+            else {
+                item.style.display = "none"
+            }
+        });
+    })
 }
