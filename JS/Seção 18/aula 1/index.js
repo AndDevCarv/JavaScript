5f1b9829efa707a1fd35c1de4b9d00bef65e49f1
andre <andredeco2003@gmail.com>
2025-06-20 17:51:15 -0300

    adicao do switch

--- JS/Seção 18/aula 1/index.js
+++ JS/Seção 18/aula 1/index.js
@@ -2,11 +2,24 @@ document.querySelector(".drum").addEventListener("click", handClick);
 
 const allButtons = document.querySelectorAll(".drum");
 
-allButtons.forEach(item => {
-    item.addEventListener("click", function(){
-        audio1.play();
+allButtons.forEach(button => {
+    button.addEventListener("keydown", (event) => {
+        const tecla = event.key.toLowerCase();
+
+        switch(tecla) {
+            case 'q':
+                audio1.play();
+                break
+            case 'a':
+                audio2.play();
+                break;
+            default:
+
+            break;
+        }
     })
+    
 })
 
 const audio1 = new Audio('sounds/tom-1.mp3')
-
+const audio2 = new Audio('sounds/tom-2.mp3')
