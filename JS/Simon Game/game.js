a55d08d71df1c34e3eed7e5361d5ddc8ad79f6da
AndDevCarv <andredeco2003@gmail.com>
2025-06-26 22:13:49 -0300

    randommmmmmmm

--- JS/Simon Game/game.js
+++ JS/Simon Game/game.js
@@ -1,1 +1,20 @@
-alert("alert");
+const buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+function nextSequence() { 
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+
+    console.log("cor sorteada", randomChosenColour);
+    console.log("sequencia atual", gamePattern);
+    
+};
+nextSequence();
+
+gamePattern.forEach(id => {
+    $(`#${id}`).click();
+});
+
+
