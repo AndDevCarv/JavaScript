bacf3f389af5b2f50b11b8e4db26fc820a21a906
AndDevCarv <andredeco2003@gmail.com>
2025-08-08 12:48:24 -0300

    ejs init

--- JS/Backend/4.0 EJS/index.js
+++ JS/Backend/4.0 EJS/index.js
@@ -1,14 +1,19 @@
 import express from "express";
+import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 
 app.listen(port, (req, res) => {
     console.log("server on port " + port);
 })
 
 app.get("/", (req, res) => {
-    
+    res.render("index");
 })
