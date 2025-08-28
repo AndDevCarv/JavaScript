a04c5c4f4f771a1b91e7e3739d6012264ec27567
AndDevCarv <andredeco2003@gmail.com>
2025-08-27 22:31:45 -0300

    finalizado

--- JS/Backend/4.2 Passing Data/index.js
+++ JS/Backend/4.2 Passing Data/index.js
@@ -4,19 +4,24 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); //bodyparser, middleware que pega informações do body, ex: nome:andre
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { //default da página, ao entrar no 127.. vai iniciar aqui
   res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
-  const fName = req.body.fName;
-  const lName = req.body.lName;
+  const firstName = req.body.fName; // pega o name do body
+  const lastName = req.body.lName; 
 
-  res.render("index.ejs", {nome: fName, sobNome: lName})
+  if (!firstName && !lastName) { //aqui estamos verificando se essas variáveis já existem, ou seja, se o campo foi preenchido, se ele nao for preenchido (!lastName) vamos lançar o index.ejs default
+    res.render("index.ejs");
+  } else{ //ja aqui, pressupondo que as variáveis foram declaradas (campos preenchidos) declaramos uma const para contagem das letras e lançamos o index.ejs com os objetos recebidos pelo bodyparser
+    const countLetter = firstName.length + lastName.length
+    res.render("index.ejs", {nome: firstName, sobNome: lastName, qtdLetra: countLetter})
+  }
 });
 
-app.listen(port, () => {
+app.listen(port, () => { //confirmação do status
   console.log(`Server running on port ${port}`);
 });
